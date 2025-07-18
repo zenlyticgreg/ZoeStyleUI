f87b50bb8ed6be276c14adc59986e79c2d2ede1c
greg@zenlytic.com <greg@zenlytic.com>
2025-07-18 10:13:10 -0700

    Improve window visibility and add debugging

--- ZenlyticStyleEditorApp.swift
+++ ZenlyticStyleEditorApp.swift
@@ -9,12 +9,17 @@ import SwiftUI
 
 @main
 struct ZenlyticStyleEditorApp: App {
+    init() {
+        print("ZenlyticStyleEditorApp: Initializing...")
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .windowStyle(.hiddenTitleBar)
+        .windowStyle(.titleBar)
         .windowResizability(.contentSize)
         .defaultSize(width: 1200, height: 800)
+        .defaultPosition(.center)
     }
 }
